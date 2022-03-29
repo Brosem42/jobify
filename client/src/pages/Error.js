7393c081da727d4c7cb6fc0de5099921109f9053
Carlos Grijalva <grijalva.carlos@gmail.com>
2022-03-29 14:51:44 +0200

    Set up register page, alert, and error components

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,8 +1,17 @@
+import { Link } from 'react-router-dom';
+import img from '../assets/images/not-found.svg';
+import Wrapper from '../assets/wrappers/ErrorPage';
+
 const Error = () => {
   return (
-    <h1>
-      Error
-    </h1>
+    <Wrapper className="full-page">
+      <div>
+        <img src={img} alt="not found"/>
+        <h3>Oh! Page not found!</h3>
+        <p>We can't seem to find the page you are looking for.</p>
+        <Link to="/">Back Home</Link>
+      </div>
+    </Wrapper>
   )
 }
 
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,8 +1,56 @@
+import { useState, useEffect } from 'react';
+import Wrapper from '../assets/wrappers/RegisterPage';
+import { Logo, FormRow, Alert } from '../components';
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+  showAlert: true,
+}
+
 const Register = () => {
+  const [values, setValues] = useState(initialState);
+  // global state and useNavigate
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+  }
+
+  const onSubmit = e => {
+    e.preventDefault();
+    console.log(e.target);
+  }
+
   return (
-    <h1>
-      Register
-    </h1>
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>Login</h3>
+        {values.showAlert && <Alert />}
+        {/* name input */}
+        <FormRow 
+          name="name"
+          type={"text"}
+          value={values.name}
+          handleChange={handleChange} 
+        />
+        <FormRow 
+          name="email"
+          type={"email"}
+          value={values.email}
+          handleChange={handleChange} 
+        />
+        <FormRow 
+          name="password"
+          type={"password"}
+          value={values.password}
+          handleChange={handleChange} 
+        />
+        <button type="submit" className="btn btn-block">submit</button>
+      </form>
+    </Wrapper>
   )
 }
 
