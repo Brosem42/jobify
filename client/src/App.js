6452a8d519e662bff7104ac28a8bf7e1478657b8
Carlos Grijalva <grijalva.carlos@gmail.com>
2022-03-29 11:29:17 +0200

    Style landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import Landing from "./pages/Landing";
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background-color: red;
+  color:white;
+`
 
 function App() {
   return (
   <div>
-    <h1>Jobify</h1>
     <Landing/>
   </div>);
 }
