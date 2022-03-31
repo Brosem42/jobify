9b29879d37d28b4a41fdc8a6073a53b426ca46ad
Carlos Grijalva <grijalva.carlos@gmail.com>
2022-03-31 13:43:37 +0200

    Hook up mongo

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
-import express from 'express'
-const app = express()
+import express from 'express';
+import dotenv from "dotenv";
+import connectDB from './db/connect.js';
+
+dotenv.config();
+
+const app = express();
+
+// middleware
+import errorHandlerMiddleware from './middleware/error-handler.js';
+import notFoundMiddleware from './middleware/not-found.js';
 
 app.get('/', (req, res) => {
   res.send('Welcome bro!')
-})
+});
+
+app.use(errorHandlerMiddleware);
+app.use(notFoundMiddleware);
 
 const port = process.env.PORT || 8000
 
-app.listen(port, () => console.log(`Server is listening on port ${port}...`))
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL)
+    app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+start()
