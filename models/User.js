b15d68a0e163373f072d678e3afba9f8bc293eff
Carlos Grijalva <grijalva.carlos@gmail.com>
2022-04-01 15:35:56 +0200

    Add JWT token

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
 import validator from "validator";
-
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -13,16 +13,16 @@ const UserSchema = new mongoose.Schema({
   },
   password: {
     type: String,
-    required: [true, 'Please provide password'],
+    required: [true, "Please provide password"],
     minlength: 6,
     select: false,
   },
-   email: {
+  email: {
     type: String,
-    required: [true, 'Please provide email'],
+    required: [true, "Please provide email"],
     validate: {
       validator: validator.isEmail,
-      message: 'Please provide a valid email',
+      message: "Please provide a valid email",
     },
     unique: true,
   },
@@ -36,13 +36,19 @@ const UserSchema = new mongoose.Schema({
     type: String,
     trim: true,
     maxLength: 20,
-    default: 'my city',
-  }
+    default: "my city",
+  },
 });
 
-UserSchema.pre('save', async function() {
+UserSchema.pre("save", async function () {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
 
-export default mongoose.model('User', UserSchema);
+UserSchema.methods.createJWT = function () {
+  return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
+  });
+};
+
+export default mongoose.model("User", UserSchema);
