dea642a3384668c92fb37fca3f3435734d333486
Carlos Grijalva <grijalva.carlos@gmail.com>
2022-04-04 11:07:52 +0200

    Set up concurrently and proxy to hook up frontend and backend

--- server.js
+++ server.js
@@ -1,46 +1,45 @@
-import express from 'express';
+import express from "express";
+import cors from "cors";
 import "express-async-errors";
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
 
-import connectDB from './db/connect.js';
+import connectDB from "./db/connect.js";
 
 // routers
-import authRouter from './routes/authRoutes.js';
+import authRouter from "./routes/authRoutes.js";
 import jobsRouter from "./routes/jobRoutes.js";
 
-
 // middleware
-import errorHandlerMiddleware from './middleware/error-handler.js';
-import notFoundMiddleware from './middleware/not-found.js';
+import errorHandlerMiddleware from "./middleware/error-handler.js";
+import notFoundMiddleware from "./middleware/not-found.js";
 
+app.use(cors());
 app.use(express.json());
 
-
-
-
-app.get('/', (req, res) => {
-  res.send('Welcome bro!')
+app.get("/api/v1", (req, res) => {
+  res.json({ msg: "welcome" });
 });
 
-app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/jobs', jobsRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", jobsRouter);
 
 app.use(errorHandlerMiddleware);
 app.use(notFoundMiddleware);
 
-const port = process.env.PORT || 8000
-
+const port = process.env.PORT || 8000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URL)
-    app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
   } catch (error) {
     console.log(error);
   }
-}
+};
 
-start()
+start();
